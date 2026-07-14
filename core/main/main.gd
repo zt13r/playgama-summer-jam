29f33d58866502f9cc7828f2884cb2d9ecea4a48
Fritz <ffztier@gmail.com>
2026-07-14 16:16:10 +0800

    Stuff

--- core/main/main.gd
+++ core/main/main.gd
@@ -1,2 +1,5 @@
 class_name Main
 extends Node
+
+
+var tiles : Dictionary[String, Dictionary] = {}
--- gameplay/player/player.gd
+++ gameplay/player/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 
 @export var move_duration : float = 0.25
 
+var input_direction : Vector2 = Vector2.ZERO
+
 var tile_size : int = 0
 var moving : bool = false
 
@@ -13,27 +15,24 @@ func _ready() -> void:
 
 
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_pressed("move_up"):
-		_move(Vector2(0, -1))
-	elif Input.is_action_pressed("move_left"):
-		_move(Vector2(-1, 0))
-	elif Input.is_action_pressed("move_down"):
-		_move(Vector2(0, 1))
-	elif Input.is_action_pressed("move_right"):
-		_move(Vector2(1, 0))
-
-	move_and_slide()
+	if input_direction.y == 0:
+		input_direction.x = Input.get_axis("move_left", "move_right")
+		_move()
+	if input_direction.x == 0:
+		input_direction.y = Input.get_axis("move_up", "move_down")
+		_move()
 
 
-func _move(direction : Vector2) -> void:
+func _move() -> void:
 	if not moving:
 		moving = true
 
+		# Actual animation
 		var tween : Tween = create_tween()
 		tween.tween_property(
 			self,
 			"position",
-			position + (direction * tile_size),
+			position + (input_direction * tile_size),
 			move_duration
 		)
 
